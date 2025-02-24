946cf21623182178a1bba955b51ec6544d99bddc
Eric Mewes <mewes.e@gmail.com>
2025-02-24 10:54:34 +0100

    Use state to toggle mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,50 +1,60 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './MobileMenu.css';
 
 const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <input
-        type="checkbox"
-        className="mobile-menu__checkbox"
-        id="menu-toggle"
-      />
-      <label htmlFor="menu-toggle" className="mobile-menu__button">
-        <span className="mobile-menu__icon"></span>
-      </label>
-      <div className="mobile-menu__overlay">
+      <button className="mobile-menu__button" onClick={toggleMenu}>
+        <span className={`mobile-menu__icon ${isOpen ? 'open' : ''}`}></span>
+      </button>
+      <div className={`mobile-menu__overlay ${isOpen ? 'open' : ''}`}>
         <nav className="mobile-menu__list">
           <NavLink
             to="/movies"
             className="mobile-menu__link"
-            data-text="Movies"
+            onClick={closeMenu}
           >
             Movies
           </NavLink>
           <NavLink
             to="/tv-shows"
             className="mobile-menu__link"
-            data-text="TV Shows"
+            onClick={closeMenu}
           >
             TV Shows
           </NavLink>
           <NavLink
             to="/Anime"
             className="mobile-menu__link mobile-menu__link--no-border"
-            data-text="Anime"
+            onClick={closeMenu}
           >
             Anime
           </NavLink>
 
           <div className="mobile-menu__spacing"></div>
 
-          <NavLink to="/history" className="mobile-menu__link">
+          <NavLink
+            to="/history"
+            className="mobile-menu__link"
+            onClick={closeMenu}
+          >
             Favorites
           </NavLink>
           <NavLink
             to="/profile"
             className="mobile-menu__link mobile-menu__link--no-border"
-            data-text="Eric"
+            onClick={closeMenu}
           >
             My Profile
           </NavLink>
