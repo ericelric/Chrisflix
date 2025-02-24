9a7bde7ddaf43d0fe3d9828262059145523b5907
Eric Mewes <mewes.e@gmail.com>
2025-02-24 22:54:37 +0100

    Use different sliders on HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,44 @@
 import useTopRatedMovies from '../../hooks/useTopRatedMovies.js';
+import useUpcomingMovies from '../../hooks/useUpcomingMovies.js';
+import useNowPlayingMovies from '../../hooks/useNowPlayingMovies.js';
+import usePopularMovies from '../../hooks/usePopularMovies.js';
 import LazySwiper from '../../components/LazySwiper/LazySwiper.jsx';
 import './HomePage.css';
 
 const HomePage = () => {
-  const { data, isLoading } = useTopRatedMovies();
+  const { data: nowPlayingMovies, isLoading: isPlayingLoading } =
+    useNowPlayingMovies();
+  const { data: upcomingMovies, isLoading: isUpcomingLoading } =
+    useUpcomingMovies();
+  const { data: topRatedMovies, isLoading: isTopRatedLoading } =
+    useTopRatedMovies();
+  const { data: popularMovies, isLoading: isPopularLoading } =
+    usePopularMovies();
 
   return (
     <>
-      <div>
-        {Array.from({ length: 10 }).map((_, i) => (
-          <LazySwiper
-            key={i}
-            index={i}
-            name={'Top Rated Movies'}
-            data={data}
-            isLoading={isLoading}
-          />
-        ))}
-      </div>
+      <LazySwiper
+        name="Now Playing Movies"
+        data={nowPlayingMovies}
+        isLoading={isPlayingLoading}
+      />
+      <LazySwiper
+        name="Upcoming Movies"
+        data={upcomingMovies}
+        isLoading={isUpcomingLoading}
+      />
+      <LazySwiper
+        name="Popular Movies"
+        data={popularMovies}
+        isLoading={isPopularLoading}
+      />
+      <LazySwiper
+        name="Top Rated Movies"
+        data={topRatedMovies}
+        isLoading={isTopRatedLoading}
+      />
     </>
   );
 };
+
 export default HomePage;
