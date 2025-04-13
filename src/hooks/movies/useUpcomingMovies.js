4232570ba2cd38d7e0965da5955baaa30df5350e
Eric Mewes <mewes.e@gmail.com>
2025-04-13 19:19:12 +0200

    Fix upcoming movies hook

--- src/hooks/movies/useUpcomingMovies.js
+++ src/hooks/movies/useUpcomingMovies.js
@@ -1,5 +1,20 @@
 import useApi from '../useApi';
 
-const useUpcomingMovies = () => useApi('/movie/upcoming?language=en-US&page=1');
+const formatDate = (date) => date.toISOString().split('T')[0];
+
+const useUpcomingMovies = () => {
+  const today = new Date();
+  const sixMonthsLater = new Date();
+  sixMonthsLater.setMonth(today.getMonth() + 6);
+
+  const minDate = formatDate(today);
+  const maxDate = formatDate(sixMonthsLater);
+
+  const endpoint = `/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&primary_release_date.gte=${minDate}&primary_release_date.lte=${maxDate}`;
+
+  const { data, loading, error } = useApi(endpoint);
+
+  return { data, loading, error };
+};
 
 export default useUpcomingMovies;
